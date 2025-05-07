f0ad19baea6d6be21c0030bdcc3d7bcb4eede0ff
gwshim <gwshim@gmail.com>
2025-05-07 13:02:21 +0900

    🎨 Tests filenaming change
    tmp_prefix add

--- ai_project/tests/test_coffee_recommendation.py
+++ ai_project/tests/test_coffee_recommendation.py
@@ -8,8 +8,20 @@ class TestCoffeeRecommendation(unittest.TestCase):
 
     def test_recommend(self):
         user_profile = {
-            "caffeine_sensitivity": 70,
-            "preferred_flavor": "bitter"
+            "gender": 1,
+            "age": 27,
+            "height": 165.0,
+            "weight": 45.1,
+            "is_smoker": 1,
+            "take_hormonal_contraceptive": 0,
+            "caffeine_sensitivity": 90,
+            "current_caffeine": 340,
+            "caffeine_limit": 400,
+            "residual_at_sleep": 38.7,
+            "target_residual_at_sleep": 50.0,
+            "planned_caffeine_intake": 125,
+            "current_time": 15.5,
+            "sleep_time": 17.5
         }
         recommendations = self.recommender.recommend(user_profile)
         self.assertIsInstance(recommendations, list)
